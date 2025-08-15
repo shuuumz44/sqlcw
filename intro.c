79bbfe3248e3fa9441d4da6cb67e8a37afc4211a
d44 <JakeSchumacher@proton.me>
2025-08-14 21:47:32 -0400

    example

--- intro.c
+++ intro.c
@@ -1,21 +1,46 @@
 #include "sqlite3.h"
 #include <stdio.h>
 
-// compilation: gcc shell.c sqlite3.c -lpthread -ldl -lm -o filename
+// compilation: gcc -o filename.exe filename.c libsqlite3.a
+
+static int callback(void *data, int argc, char **argv, char **azColName) {
+    int i;
+    for (i=0; i < argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : NULL);
+    }
+    printf("\n");
+    return 0;
+}
 
 int main(int argc, char **argv) {
     sqlite3 *db;
+    char *zErrMsg = 0;
     int rc;
 
-    rc = sqlite3_open(argv[1], &db);
+    if (argc != 3) {
+        printf("usage: [DATABASE] [TABLE]\n");
+        return 1;
+    }
 
+    rc = sqlite3_open(argv[1], &db);
     if (rc != SQLITE_OK) {
         printf("database could not be opened.\n");
         sqlite3_close(db);
         return 2;
     }
+    printf("database opened.\n\n");
 
-    printf("database opened.\n");
+    char sql[100];
+    sprintf(sql, "SELECT * FROM %s LIMIT 1;", argv[2]);
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        sqlite3_close(db);
+        return 3;
+    }
+    printf("data callbacked.\n");
+
     sqlite3_close(db);
     return 0;
 }
